e93e55246d5238f379b5960b610d308da032c287
Lionheart33 <ricardo.jnf1@gmail.com>
2024-04-27 19:22:43 -0300

    add tradução

--- src/instagram/main.py
+++ src/instagram/main.py
@@ -6,7 +6,7 @@ def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
         'current_date': datetime.datetime.now().strftime("%Y-%m-%d"),
-        'instagram_description': input('Enter the page description here: '),
-        'topic_of_the_week': input('Enter the topic of the week here: '),
+        'instagram_description': input('Escreva a descrição da página aqui: '),
+        'topic_of_the_week': input('Escreva o tópico da semana aqui: '),
     }
     InstagramCrew().crew().kickoff(inputs=inputs)
